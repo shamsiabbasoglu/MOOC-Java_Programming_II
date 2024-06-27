cfb28f07ca3c4b3d005d9d12e6d183a80e21adf3
Şəmsi Abbasoğlu <shamsi.abbasov@hotmail.com>
2024-06-28 00:17:14 +0400

    Describing inheritance

---
+++ Part11/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,11 @@
+
+public class Bot extends Player {
+
+    @Override
+    public void play() {
+        super.play(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addMove(String move) {
+    }
+}
